f5e49d2017c73a5a5a29f91706a4dfccad6e7698
lizhimin <lizhimin@metersbonwe.com>
2019-12-11 10:25:58 +0800

    1

--- hello.py
+++ hello.py
@@ -52,7 +52,7 @@ class AlchemyEncoder(json.JSONEncoder):
                     json.dumps(data)    # this will fail on non-encodable values, like other classes
                     fields[field] = data
                 except TypeError:    # 添加了对datetime的处理
-                    # print(type(data),data)
+                    print(type(data),data)
                     if isinstance(data, datetime.datetime):
                         fields[field] = data.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] # SQLserver数据库中毫秒是3位，日期格式;2015-05-12 11:13:58.543
                     elif isinstance(data, datetime.date):
