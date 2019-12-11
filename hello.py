e45cf71dd033f4da7bb25befd3d2f79c2e3bfc43
lizhimin <lizhimin@metersbonwe.com>
2019-12-11 09:20:49 +0800

    1

--- hello.py
+++ hello.py
@@ -38,13 +38,6 @@ class plan_price_ranges(db.Model):
 		if "_sa_instance_state" in dict:
 			del dict["_sa_instance_state"]
 		return dict
-	
-	# list 转成Json格式数据
-	def listToJson(lst):
-    keys = [str(x) for x in np.arange(len(lst))]
-    list_json = dict(zip(keys, lst))
-    str_json = json.dumps(list_json, indent=2, ensure_ascii=False)  # json转为string
-    return str_json
 
 #显示所有数据
 @app.route('/')
@@ -80,12 +73,12 @@ def comments():
 
 @app.route('/get_category_sum', methods=['GET'])
 def get_category_sum():
-		#rs = plan_price_ranges.query.with_entities(func.sum(plan_price_ranges.unit)).all()
-		rs = db.session.query(plan_price_ranges.pclass, func.count(plan_price_ranges.unit)).group_by(plan_price_ranges.pclass).all()
-		result1 = []
-		#for line1 in rs:
-		#	result1.append(line1.to_json())
-		result1 = listToJson(rs)
+		plan_price_ranges1 = plan_price_ranges.query.with_entities(func.sum(plan_price_ranges.unit)).all()
+		#rs = db.session.query(plan_price_ranges.pclass, func.count(plan_price_ranges.unit)).group_by(plan_price_ranges.pclass).all()
+		result = []
+    for plan_price_range in plan_price_ranges1:
+        result.append(plan_price_range.to_json())
+    return jsonify({'rows': result})
 		return result1
         
 #获取全部数据的分类汇总透视表
