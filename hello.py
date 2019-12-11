7d1fd65c55221f2321616fd98d972053010530d7
lizhimin <lizhimin@metersbonwe.com>
2019-12-11 10:04:06 +0800

    1

--- hello.py
+++ hello.py
@@ -41,36 +41,29 @@ class plan_price_ranges(db.Model):
 			del dict["_sa_instance_state"]
 		return dict
 
-def new_alchemy_encoder():
-    _visited_objs = []
-
-    class AlchemyEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj.__class__, DeclarativeMeta):
-                # don't re-visit self
-                if obj in _visited_objs:
-                    return None
-                _visited_objs.append(obj)
-
-                # an SQLAlchemy class
-                fields = {}
-                for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                    data = obj.__getattribute__(field)
-                    try:
-                    	  if isinstance(data, decimal.Decimal):
-                            data = float(data)
-                            json.dumps(ret_list, cls=DecimalEncoder)
-                        else:
-                            if isinstance(data, datetime):
-                                data = data.strftime('%Y-%m-%d %H:%M:%S')
-                            json.dumps(data)  # this will fail on non-encodable values, like other classes
-                        fields[field] = data
-                    except TypeError:
-                        fields[field] = None
-                return fields
-
-            return json.JSONEncoder.default(self, obj)
-    return AlchemyEncoder
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data)     # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:    # 添加了对datetime的处理
+                    # print(type(data),data)
+                    if isinstance(data, datetime.datetime):
+                        fields[field] = data.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] #SQLserver数据库中毫秒是3位，日期格式;2015-05-12 11:13:58.543
+                    elif isinstance(data, datetime.date):
+                        fields[field] = datadata.strftime("%Y-%m-%d")
+                    elif isinstance(data, decimal.Decimal):
+                        fields[field]= float(data)                        
+                    else:
+                        fields[field] = AlchemyEncoder.default(self, data) #如果是自定义类，递归调用解析JSON，这个是对象映射关系表 也加入到JSON
+            # a json-encodable dict
+            return fields
+        return json.JSONEncoder.default(self, obj)
     
 #显示所有数据
 @app.route('/')
@@ -110,7 +103,8 @@ def get_category_sum():
     msgs = []
     for msg in UnReadMsg:
         msgs.append(msg)
-    UnReadMsg = json.dumps(msgs, cls=new_alchemy_encoder(), check_circular=False) 
+    rts = json.dumps(msgs, cls=AlchemyEncoder,ensure_ascii=False)
+    return rts
         
 #获取全部数据的分类汇总透视表
 @app.route('/newppr', methods=['GET'])
